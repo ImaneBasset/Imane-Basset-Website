438af08e8d32de013de5b6aec49f06e4d98a08a0
Basset Imane <ixb200@student.bham.ac.uk>
2025-11-14 20:55:53 +0000

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -80,7 +80,7 @@ const Hero = () => {
               <Linkedin size={24} />
             </a>
             <a
-              href="https://github.com"
+              href="https://github.com/ImaneBasset"
               target="_blank"
               rel="noopener noreferrer"
               className="text-muted-foreground hover:text-primary transition-smooth"
